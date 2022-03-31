91b8eaf68a3358d5f9aba0cdbf16e0de4baa9af7
HyungchulCha <ckgudcjf@gmail.com>
2022-03-31 22:27:31 +0900

    ing

--- 03/02/src/Say.js
+++ 03/02/src/Say.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
 
 const Say = () => {
-    const [message, setMessage] = useState('');
-    const onClickEnter = () => setMessage('hi');
-    const onClickLeave = () => setMessage('bye');
+  const [message, setMessage] = useState('');
+  const onClickEnter = () => setMessage('hi');
+  const onClickLeave = () => setMessage('bye');
+  const [color, setColor] = useState('black');
 
-    return (
-        <div>
-            <h1>{message}</h1>
-            <button onClick={onClickEnter}>enter</button>
-            <button onClick={onClickLeave}>leave</button>
-        </div>
-    );
+  return (
+    <div>
+      <h1 style={{ color }}>{message}</h1>
+      <button onClick={onClickEnter}>enter</button>
+      <button onClick={onClickLeave}>leave</button>
+      <button onClick={() => setColor('red')}>red</button>
+      <button onClick={() => setColor('green')}>green</button>
+      <button onClick={() => setColor('blue')}>blue</button>
+    </div>
+  );
 };
 
 export default Say;
