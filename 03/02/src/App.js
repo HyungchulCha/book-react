b435c3bfd745675a8a26a4a621de17b0b397b993
HyungchulCha <ckgudcjf@gmail.com>
2022-03-31 21:03:40 +0900

    ing

--- 03/02/src/App.js
+++ 03/02/src/App.js
@@ -2,12 +2,12 @@ import Counter from './Counter';
 import Say from './Say';
 
 function App() {
-    return (
-        <div>
-            <Counter />
-            <Say />
-        </div>
-    );
+  return (
+    <div>
+      <Counter />
+      <Say />
+    </div>
+  );
 }
 
 export default App;
