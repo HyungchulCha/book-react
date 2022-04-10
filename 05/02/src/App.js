39fe7c1ba93c6aacaf315e58d56c392a30ed2f0e
HyungchulCha <ckgudcjf@gmail.com>
2022-04-10 21:43:20 +0900

    ing

---
+++ 05/02/src/App.js
@@ -0,0 +1,12 @@
+import RefSample from './RefSample';
+import ValidationSample from './ValidationSample';
+function App() {
+  return (
+    <div>
+      <ValidationSample />
+      <RefSample />
+    </div>
+  );
+}
+
+export default App;
--- 05/02/src/ValidationSample.js
+++ 05/02/src/ValidationSample.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './ValidationSample.css';
 
 class ValidationSample extends Component {
+  input = React.createRef();
+  handleFocus = () => {
+    this.input.current.focus();
+  };
   state = {
     password: '',
     clicked: false,
@@ -17,11 +21,12 @@ class ValidationSample extends Component {
       clicked: true,
       validated: this.state.password === '000',
     });
+    this.input.focus();
   };
   render() {
     return (
       <div>
-        <input type='password' value={this.state.password} onChange={this.handleChange} className={this.state.clicked ? (this.state.validated ? 'success' : 'failure') : ''} />
+        <input type='password' ref={(ref) => (this.input = ref)} value={this.state.password} onChange={this.handleChange} className={this.state.clicked ? (this.state.validated ? 'success' : 'failure') : ''} />
         <button onClick={this.handleButtonClick}>Validation</button>
       </div>
     );
